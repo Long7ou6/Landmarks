dcdd7ad82dece7169786c9b43a065eeec85b9d85
Long7ou6 <lehailong2509@gmail.com>
2024-05-15 21:13:10 +0700

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,20 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Long on 16/4/24.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    @State private var modelData = ModelData()
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(modelData)
+        }
+    }
+}
